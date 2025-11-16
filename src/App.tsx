f9ab8e1d422248f7a0b8abb8d63399cfca584cde
Bilal90738 <130696819+Bilal90738@users.noreply.github.com>
2025-11-16 22:50:14 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+  <div className="flex flex-col justify-center items-center py-5 px-2 sm:px-10 border-2 md:mx-auto  " >
+    <h1 className='text-4xl font-bold text-orange-400'>TES<u className='underline-offset-20'>TIMONIA</u>LS</h1>
+    <p className='mt-10 font-semibold text-gray-500 text-center'>Subscribe Easy Tutorial Youtube channel to watch more</p>
+
+    <div className="flex flex-col sm:flex-row flex-wrap justify-center items-center gap-5 mx-auto mt-0 lg:mt-40  ">
+
+      <div className="card relative bg-gray-300/50 w-[300px] sm:w-[370px] pt-20 px-10 pb-10 hover:shadow-2xl cursor-pointer mt-20 xl:mt-0 ">
+        <p className='text-xl'><span className='text-orange-400 font-bold text-xl'>“</span> Like this video and ask your questions in commentsection , don't forget to subscribe. Esay tutuorials Youtube channel to watch more videos of website designing  digital marketing and photoshop. <span className='text-orange-400 font-bold text-xl '>”</span></p>
+        <p className='text-center mt-5 '><b className='text-orange-400 text-xl'>Avinash kr</b> <span className='text-[11px]'>Co-Founder at xyz</span> </p>
+        <div>
+          <img src="/images/user1.jpeg" className='absolute top-[-65px] left-1/2 -translate-x-1/2 h-30 rounded-full border-8 border-white' alt="" />
+        </div>
+      </div>
+
+      <div className="card relative bg-gray-300/50 w-[300px] sm:w-[370px] pt-20 px-10 pb-10 hover:shadow-2xl cursor-pointer mt-20 xl:mt-0">
+        <p className='text-xl'><span className='text-orange-400 font-bold text-xl'>“</span> Like this video and ask your questions in commentsection , don't forget to subscribe. Esay tutuorials Youtube channel to watch more videos of website designing  digital marketing and photoshop. <span className='text-orange-400 font-bold text-xl '>”</span></p>
+        <p className='text-center mt-5 '><b className='text-orange-400 text-xl'>Avinash kr</b> <span className='text-[11px]'>Co-Founder at xyz</span> </p>
+        <div>
+          <img src="/images/user1.jpeg" className='absolute top-[-65px] left-1/2 -translate-x-1/2 h-30 rounded-full border-8 border-white' alt="" />
+        </div>
+      </div>
+
+      <div className="card relative bg-gray-300/50 w-[300px] sm:w-[370px] pt-20 px-10 pb-10 hover:shadow-2xl cursor-pointer mt-20 xl:mt-0">
+        <p className='text-xl'><span className='text-orange-400 font-bold text-xl'>“</span> Like this video and ask your questions in commentsection , don't forget to subscribe. Esay tutuorials Youtube channel to watch more videos of website designing  digital marketing and photoshop. <span className='text-orange-400 font-bold text-xl '>”</span></p>
+        <p className='text-center mt-5 '><b className='text-orange-400 text-xl'>Avinash kr</b> <span className='text-[11px]'>Co-Founder at xyz</span> </p>
+        <div>
+          <img src="/images/user1.jpeg" className='absolute top-[-65px] left-1/2 -translate-x-1/2 h-30 rounded-full border-8 border-white' alt="" />
+        </div>
+      </div>
+    </div>
+  </div>
+  </>
+  )
+}
+
+export default App
