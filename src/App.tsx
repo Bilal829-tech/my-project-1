5069af8f1c19a166acaa30159d1b1cf49411ebb3
Bilal90738 <130696819+Bilal90738@users.noreply.github.com>
2025-11-16 22:59:42 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <h1 className='text-4xl font-bold text-orange-400'>TES<u className='underline-offset-20'>TIMONIA</u>LS</h1>
     <p className='mt-10 font-semibold text-gray-500 text-center'>Subscribe Easy Tutorial Youtube channel to watch more</p>
 
-    <div className="flex flex-col sm:flex-row flex-wrap justify-center items-center gap-5 mx-auto mt-0 lg:mt-40  ">
+    <div className="flex flex-col sm:flex-row flex-wrap justify-center items-center gap-5 mx-auto mt-0 lg:mt-30  ">
 
       <div className="card relative bg-gray-300/50 w-[300px] sm:w-[370px] pt-20 px-10 pb-10 hover:shadow-2xl cursor-pointer mt-20 xl:mt-0 ">
         <p className='text-xl'><span className='text-orange-400 font-bold text-xl'>“</span> Like this video and ask your questions in commentsection , don't forget to subscribe. Esay tutuorials Youtube channel to watch more videos of website designing  digital marketing and photoshop. <span className='text-orange-400 font-bold text-xl '>”</span></p>
